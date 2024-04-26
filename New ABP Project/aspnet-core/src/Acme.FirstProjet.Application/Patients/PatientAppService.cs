84fa4b5ae58978a7ccd1fac8774e9773f2dd8c21
Baraa-Baghdadi <barrasy19999@gmail.com>
2024-04-26 17:04:46 +0200

    Edit Tenancy

--- New ABP Project/aspnet-core/src/Acme.FirstProjet.Application/Patients/PatientAppService.cs
+++ New ABP Project/aspnet-core/src/Acme.FirstProjet.Application/Patients/PatientAppService.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
 
 namespace Acme.FirstProjet.Patients
 {
@@ -13,9 +16,11 @@ namespace Acme.FirstProjet.Patients
   public class PatientAppService : ApplicationService, IPatientAppService
     {
         private readonly IPatientProviderRepository _patientProviderRepository;
-        public PatientAppService(IPatientProviderRepository patientProviderRepository)
+        private readonly IDataFilter _dataFilter;
+        public PatientAppService(IPatientProviderRepository patientProviderRepository, IDataFilter dataFilter)
         {
-            _patientProviderRepository = patientProviderRepository;
+                _patientProviderRepository = patientProviderRepository;
+                _dataFilter = dataFilter;
         }
 
 
@@ -23,15 +28,13 @@ namespace Acme.FirstProjet.Patients
         [Authorize]
         public async Task<PagedResultDto<PatientProviderDto>> GetAllPatientsOfProviderAsync(GetPatientInput input)
         {
-
-
             var totalCount = await _patientProviderRepository.GetCountAsync(input.FilterText, input.MobileNumber, input.CountryCode);
             var items = await _patientProviderRepository.GetListAsync(input.FilterText, input.MobileNumber, input.CountryCode,
                 input.Sorting, input.MaxResultCount, input.SkipCount);
             return new PagedResultDto<PatientProviderDto>
             {
-                TotalCount = totalCount,
-                Items = ObjectMapper.Map<List<PatientProvider>, List<PatientProviderDto>>(items)
+              TotalCount = totalCount,
+              Items = ObjectMapper.Map<List<PatientProvider>, List<PatientProviderDto>>(items)
             };
         }
 
